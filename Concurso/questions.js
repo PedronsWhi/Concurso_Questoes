404605a2344d403ba68756752707e077ff344363
PEDRO SÉRGIO DOS SANTOS CAMPOS <pedro.campos@a.ucb.br>
2024-05-22 11:02:43 -0300

    Concurso

---
+++ Concurso/questions.js
@@ -0,0 +1,37 @@
+export default [
+    {
+      question: "Getúlio Vargas foi um dos grandes presidentes do Brasil, conhecido como 'o pai dos pobres'. Por qual obra ele é lembrado dessa maneira?",
+      answers: [
+        { option: "Por ter criado leis para o voto de todos e para os trabalhadores rurais.", correct: true },
+        { option: "Por ser um liberalista exemplar e ter elevado a economia brasileira a ser lembrada no contexto internacional.", correct: false },
+        { option: "Por ser uma referência democrática do Brasil.", correct: false },
+      ],
+    },
+    {
+      question: "João aplicou R$ 5.000,00 em um fundo de investimento que rende juros compostos a uma taxa de 8% ao ano. Quanto ele terá ao final de 2 anos?",
+        
+      
+  
+      answers: [
+        { option: " R$ 6.400,00", correct: false },
+        { option: "R$ 5.824,00", correct: true },
+        { option: "R$ 5.840,00", correct: false },
+      ],
+    },
+    {
+      question: "Qual alternativa apresenta concordância nominal correta?",
+      answers: [
+        { option: "As menina estava felizes.", correct: false },
+        { option: "Os livro era interresante.", correct: false },
+        { option: "Os meninos estavam felizes.", correct: true },
+      ],
+    },
+    {
+      question: "Quais o menor e o maior país do mundo?",
+      answers: [
+        { option: "Nauru e China.", correct: false },
+        { option: "Malta e Estados Unidos.", correct: false },
+        { option: "Vaticano e Rússia.", correct: true },
+      ],
+    },
+  ];
